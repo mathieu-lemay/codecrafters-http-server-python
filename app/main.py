c4bc293e7629e285028a985675a52e87a6454263
Mathieu Lemay <acidrain1@gmail.com>
2023-10-08 17:01:06 -0400

    Stage 2

--- app/main.py
+++ app/main.py
@@ -3,7 +3,17 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    while True:
+        conn, addr = server_socket.accept()
+
+        print(f"connection from {addr[0]}:{addr[1]}")
+        with conn:
+            handle(conn)
+            conn.shutdown(socket.SHUT_RDWR)
+
+
+def handle(conn: socket.socket) -> None:
+    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
