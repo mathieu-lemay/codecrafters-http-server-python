0ee3eca3782567c4795bd2768667206e8c28cb5a
Mathieu Lemay <acidrain1@gmail.com>
2023-10-08 17:22:41 -0400

    Stage 3

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
+from dataclasses import dataclass
 import socket
+from http import HTTPStatus
+from uuid import uuid4
+
+
+@dataclass
+class Request:
+    method: str
+    path: str
+    headers: dict[str, str]
 
 
 def main():
@@ -7,13 +17,69 @@ def main():
         conn, addr = server_socket.accept()
 
         print(f"connection from {addr[0]}:{addr[1]}")
-        with conn:
+        try:
             handle(conn)
-            conn.shutdown(socket.SHUT_RDWR)
+        except Exception as e:
+            print(f"Error handling connection: {e}")
 
 
 def handle(conn: socket.socket) -> None:
-    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    req_id = uuid4()
+    request = parse_request(conn)
+    print(f"{req_id} - Request: {request}")
+
+    if request.path == "/":
+        resp = build_empty_response(HTTPStatus.OK)
+    else:
+        resp = build_empty_response(HTTPStatus.NOT_FOUND)
+
+    print(f"{req_id} - raw response:\n======\n{resp}\n======\n")
+    conn.sendall(resp)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+    print(f"{req_id} - closed conn")
+
+
+def parse_request(conn: socket.socket) -> Request:
+    data = conn.recv(2048)
+    print(f"raw request:\n======\n{data}\n======\n")
+
+    start_line, *elements = data.split(b"\r\n")
+
+    method, path, _ = start_line.decode().split(" ")
+    headers = {}
+
+    elem_iter = iter(elements)
+    while header := next(elem_iter).decode():
+        k, v = header.split(": ", maxsplit=1)
+        headers[k.lower()] = v
+
+    _body = next(elem_iter, None)
+
+    return Request(method=method, path=path, headers=headers)
+
+
+def build_empty_response(status_code: HTTPStatus) -> bytes:
+    return build_response(status_code)
+
+
+def build_response(
+    status_code: HTTPStatus, headers: dict[str, str] | None = None, body: bytes = b""
+) -> bytes:
+    status = f"HTTP/1.1 {status_code} {status_code.phrase}\r\n"
+    headers = {
+        "Content-Type": "text/plain",
+        "Content-Length": len(body),
+        **(headers or {}),
+    }
+
+    return (
+        status.encode()
+        + "\r\n".join(f"{k}: {v}" for k, v in headers.items()).encode()
+        + b"\r\n"
+        + body
+        + b"\r\n"
+    )
 
 
 if __name__ == "__main__":
